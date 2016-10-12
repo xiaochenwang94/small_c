59b488166b9788338f9e94320bacd69b2fdf4435
wxc575843 <1040142133@qq.com>
2016-10-12 11:04:47 +0800

    Initial Commit

---
+++ small_c/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  small_c
+//
+//  Created by Wang Bill on 16/10/12.
+//  Copyright © 2016年 Wang Bill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
